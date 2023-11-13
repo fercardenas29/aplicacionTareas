8cac045566f19eae30ccc7173b6c92bb6e4d6c58
diegow7 <diegorafaelariassarango7@gmail.com>
2023-11-12 23:15:15 -0500

    A comment is added to main

--- src/Paquete/Main.java
+++ src/Paquete/Main.java
@@ -17,7 +17,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        System.out.println("Para ver si funca el cambio");
+        System.out.println("Para ver si funca el cambio 10.0");
         //Llamado a ventana principal
         VentanaMain ventanaMain = new VentanaMain();
         ventanaMain.setVisible(true);
